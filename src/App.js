ad6c7b4b1b58c8f4270f3cf11df974c5b372253a
matejdostal <matej.dostal.sk@gmail.com>
2021-06-03 11:45:00 +0200

    ...

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import GoogleMap, { initializeApi } from "./GoogleMap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+initializeApi(
+    {
+        // apiKey: "{GOOGLE_MAPS_JS_API_KEY}",
+        // https://googlemaps.github.io/js-api-loader/interfaces/loaderoptions.html
+    },
+    {
+        // https://developers.google.com/maps/documentation/javascript/reference/map#MapOptions
+    }
+);
+
+const SCREENS_COUNT = 5;
+
+const App = () => {
+    const [screenIndex, setScreenIndex] = useState(0);
+
+    const createMapHandler = (map) => {
+        // console.log("createMapHandler", map);
+        if (screenIndex === 1) {
+            map.setOptions({
+                center: { lat: 48, lng: 17 },
+                zoom: 8,
+                mapTypeId: "hybrid",
+            });
+        }
+        if (screenIndex === 3) {
+            map.setOptions({
+                center: { lat: 50, lng: 15 },
+                zoom: 12,
+                mapTypeId: "roadmap",
+            });
+        }
+    };
+
+    const destroyMapHandler = (map) => {
+        // console.log("destroyMapHandler", map);
+    };
+
+    const menuButtons = [];
+    for (let i = 0; i < SCREENS_COUNT; i++) {
+        menuButtons.push(
+            <button key={i} className="menu-button" onClick={() => setScreenIndex(i)} disabled={i === screenIndex}>
+                Screen #{i}
+            </button>
+        );
+    }
+    const currentScreen = (
+        <div className="screen">
+            <h3>Screen #{screenIndex}</h3>
+            {screenIndex % 2 === 1 && (
+                <div style={{ width: 600, height: 400, border: "1px solid black" }}>
+                    <GoogleMap key={screenIndex} onCreate={createMapHandler} onDestroy={destroyMapHandler} />
+                </div>
+            )}
+        </div>
+    );
+    return (
+        <div className="app-container">
+            <div className="menu-container">{menuButtons}</div>
+            <div className="screen-container">{currentScreen}</div>
+        </div>
+    );
+};
 
 export default App;
---
+++ src/GoogleMap.js
@@ -0,0 +1,69 @@
+import { useState, useEffect, useRef } from "react";
+import { Loader } from "@googlemaps/js-api-loader";
+
+// -------------------------------------------------------------------
+
+let loaderOptions = {};
+let defaultMapOptions = {};
+
+const instances = [];
+
+const initializeApi = (
+    _loaderOptions, // https://googlemaps.github.io/js-api-loader/interfaces/loaderoptions.html
+    _defaultMapOptions // https://developers.google.com/maps/documentation/javascript/reference/map#MapOptions
+) => {
+    loaderOptions = _loaderOptions;
+    defaultMapOptions = _defaultMapOptions || {};
+};
+
+const createInstance = () => {
+    const element = document.createElement("div");
+    element.style.width = "100%";
+    element.style.height = "100%";
+    return new window.google.maps.Map(element, defaultMapOptions);
+};
+
+const getInstance = () => {
+    return instances.length > 0 ? instances.pop() : createInstance();
+};
+
+const getInstanceAsync = async () => {
+    return new Loader(loaderOptions).load().then(() => getInstance());
+};
+
+const releaseInstance = (instance) => {
+    instances.push(instance);
+};
+
+export { initializeApi };
+
+// -------------------------------------------------------------------
+
+const GoogleMap = ({ onCreate, onDestroy }) => {
+    const [map, setMap] = useState(null);
+    const wrapperElRef = useRef(null);
+
+    useEffect(() => {
+        if (!map) {
+            getInstanceAsync().then((instance) => setMap(instance));
+        } else {
+            if (wrapperElRef.current) {
+                wrapperElRef.current.appendChild(map.getDiv());
+                onCreate(map);
+            }
+        }
+        return () => {
+            if (map) {
+                if (map.getDiv() && map.getDiv().parentElement) {
+                    map.getDiv().parentElement.removeChild(map.getDiv());
+                }
+                onDestroy(map);
+                releaseInstance(map);
+            }
+        };
+    }, [map, wrapperElRef]);
+
+    return <div style={{ width: "100%", height: "100%" }} ref={wrapperElRef}></div>;
+};
+
+export default GoogleMap;
